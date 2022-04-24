350706a07b93cd716bbd9353ae73aa06a13102c3
yu <mark2020.yu@gmail.com>
2022-04-25 02:18:59 +0800

    add heroku app hostname

--- routes/ip.js
+++ routes/ip.js
@@ -1,21 +1,21 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 const fetch = require("cross-fetch");
 
 /* GET web server external ip address. */
-router.get('/', async function(req, res, next) {
-    const url = "https://api.ipify.org?format=json";
-    const myHost = "polar-reaches-82211.herokuapp.com";
-    fetch(url)
-        .then((response) => response.json())
-        .then((data) => {
-            console.log(data);
-            res.status(200).json({
-                host: myHost,
-                ip: data.ip,
-              })
-        })
-        .catch((error) => console.log(error, 2));
-  });
+router.get("/", async function (req, res, next) {
+	const url = "https://api.ipify.org?format=json";
+	const myHost = "calm-tor-51704.herokuapp.com";
+	fetch(url)
+		.then((response) => response.json())
+		.then((data) => {
+			console.log(data);
+			res.status(200).json({
+				host: myHost,
+				ip: data.ip,
+			});
+		})
+		.catch((error) => console.log(error, 2));
+});
 
 module.exports = router;
